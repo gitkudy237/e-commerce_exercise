dec38850437697837f37be05c5a490add677acb6
Kudy <bayardkudy1@gmail.com>
2025-12-04 16:18:05 +0100

    Create api service

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./config/database");
 const ProductModel = require("./models/productModel");
 const productsRouter = require("./routes/products");
@@ -13,6 +14,7 @@ sequelize
   .then(() => console.log("Database synced"))
   .catch((err) => console.error("Sync failed: ", err.message));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("./"));
 app.use("/api/products", productsRouter);
