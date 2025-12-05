e2a14d8a13979e5a48ea3cb93780b3c67b67e32f
willy11001 <tembewilliamtembewilliam@gmail.com>
2025-12-05 10:14:44 +0100

    new

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
   res.send("Backend API is running...");
 });
 
-// start server new server
+// start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
