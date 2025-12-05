76ce0512a5611393e802a12579125f189991184a
willy11001 <tembewilliamtembewilliam@gmail.com>
2025-12-05 09:26:52 +0100

    new commit

--- FRONT-END/src/App.vue
+++ FRONT-END/src/App.vue
@@ -4,7 +4,7 @@
     <nav class="navbar">
       <h1 class="logo">
         <img src="./assets/logo.webp" alt="" />
-        MiniShop WILLIAM
+        MiniShop
       </h1>
 
       <div class="nav-links">
